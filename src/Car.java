98c7bb449b440a27d906d2c94a6b7fd1814ad0db
Jade Johnson <jade@MacBook-Pro-13.NorwayHouse>
2026-04-22 14:07:50 -0500

    ShopManager: added carGet(), carShow(), carShow(String nickname), carAdd(), carAdvance()   Car: added workStageAdvance()

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,8 @@
 public class Car {
     /* fields */
-    private String nickname;
-    private String make;
-    private String model;
+    private String nickname = "none";
+    private String make = "none";
+    private String model = "none";
     private int year;
     private int workStage;
     private boolean isComplete;
@@ -10,13 +10,12 @@ public class Car {
     /* constructors */
     Car() {
     }
-    Car(String nickname, String make, String model, int year, int workStage, boolean isComplete) {
+    Car(String nickname, String make, String model, int year, int workStage) {
         setNickname(nickname);
         setMake(make);
         setModel(model);
         setYear(year);
         setWorkStage(workStage);
-        setIsComplete(isComplete);
     }
 
     //toString override default object and return a report of every field value
@@ -28,8 +27,18 @@ public class Car {
         sb.append("Model: ").append(model).append('\n');
         sb.append("Year: ").append(year).append('\n');
         sb.append("Stage of work: ").append(workStage).append('\n');
-        sb.append("Completed?: ").append(isComplete).append('\n');
+        sb.append("Done: ").append(isComplete).append('\n');
         return sb.toString();
+    }
+
+    public void workStageAdvance() {
+        if (workStage < 4) {
+            workStage += 1;
+            System.out.println("Car advanced to stage: " + workStage + "\n");
+        }
+        else {
+            System.out.println("Car is already complete!");
+        }
     }
 
     /* getters and setters */
@@ -45,8 +54,22 @@ public class Car {
     public void setYear(int year) {
         this.year = year;
     }
+    /*
+    *
+    * 0 = ready to start bodywork
+    * 1 = working on bodywork
+    * 2 = bodywork finished, working on paint
+    * 3 = paint finished, ready for touchups
+    * 4+ = done
+     */
     public void setWorkStage(int stage) {
         workStage = stage;
+        if (workStage >= 4) {
+            setIsComplete(true);
+        }
+        else {
+            setIsComplete(false);
+        }
     }
     public void setIsComplete(boolean isComplete) {
         this.isComplete = isComplete;
--- src/ShopManager.java
+++ src/ShopManager.java
@@ -1,23 +1,53 @@
 import java.util.ArrayList;
+/* ArrayList and associate logic for Car and WorkLog objects*/
 public class ShopManager {
     /* declare arraylist fields */
     ArrayList<Car> cars = new ArrayList<>();
     ArrayList<WorkLog> log = new ArrayList<>();
 
 
-    /*constructor */
-    ShopManager() {
-    }
+    /*
+    * Java automatically makes a default no-arg construct
+    * declares and initializes empty ArrayLists: cars and log
+     */
 
 
-    /* cars methods */
-    //add a car
+    /*** CARS METHODS ***/
+    /* add a car object to cars */
     public void carAdd(Car car) {
         cars.add(car);
     }
-    //todo show a cars details by name
-    //todo show all cars
-    //todo advance car to next stage
+
+    /* print a car object's details from its nickname */
+    public void carShow(String nickname) {
+        for (Car car: cars) {   // loop through ArrayList cars
+            if (car.getNickname().equals(nickname)) {   //when a car with the matching nickname is found
+                System.out.println(car.toString());   //print its .toString()
+            }
+        }
+    }
+
+    /* helper method to return car object from nickname */
+    public Car carGet(String nickname) {
+        for (Car car: cars) {   // loop through ArrayList cars
+            if (car.getNickname().equals(nickname)) {   //when a car with the matching nickname is found
+                return car;
+            }
+        }
+        return null;
+    }
+
+    /* print every car and its values */
+    public void carShow() {
+        for (Car car : cars) {   // loop through cars
+            System.out.println(car.toString());   // print report
+        }
+    }
+
+    /* advance car's workStage from nickname */
+    public void carAdvance(String nickname) {
+        carGet(nickname).workStageAdvance();   // use workStageAdvance in class Car
+    }
 
 
     /* log methods */
@@ -27,4 +57,28 @@ public class ShopManager {
     //todo delete a log by index
     //todo clear all logs
     //todo return total hours worked across log entries
+
+
+    /* test method */
+    public static void main(String[] args) {
+        ShopManager shop1 = new ShopManager();
+
+        /* car tests
+        Car car1 = new Car("stang", "Ford", "Mustang: 69 Coupe", 1969, 0);
+        Car car2 = new Car();
+        shop1.carAdd(car1);
+        shop1.carAdd(car2);
+        shop1.carShow("stang");
+        System.out.println(shop1.carGet("stang").toString());
+
+        for (int i = 0; i < 5; i++) {
+            shop1.carAdvance("stang");
+        }
+         */
+
+
+
+
+
+    }
 }
